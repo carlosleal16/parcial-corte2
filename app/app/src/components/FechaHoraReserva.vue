f7217600ff5d51ec8f54c89902350fb7044b2c54
carlosleal16 <cmleal-2023a@corhuila.edu.co>
2025-04-23 01:17:04 -0500

    Componente de selección de fecha y hora

---
+++ app/app/src/components/FechaHoraReserva.vue
@@ -0,0 +1,18 @@
+<template>
+    <ion-item>
+      <ion-label>Fecha y Hora</ion-label>
+      <ion-datetime v-model="fechaHora" presentation="date-time"></ion-datetime>
+    </ion-item>
+  </template>
+  
+  <script setup>
+  import { ref, defineEmits, watch } from 'vue';
+  
+  const emit = defineEmits(['update:fechaHora']);
+  const fechaHora = ref(new Date().toISOString());
+  
+  watch(fechaHora, (val) => {
+    emit('update:fechaHora', val);
+  });
+  </script>
+  
