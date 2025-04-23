942cafbbbd84c7fe19646279dcfd2387ce2e93f9
carlosleal16 <cmleal-2023a@corhuila.edu.co>
2025-04-23 01:30:17 -0500

    Componente para escoger la mesa

---
+++ app/app/src/components/SeleccionMesa.vue
@@ -0,0 +1,22 @@
+<template>
+    <ion-list>
+      <ion-radio-group v-model="mesaSeleccionada" @ionChange="emit('update:mesa', mesaSeleccionada)">
+        <ion-item v-for="mesa in mesasDisponibles" :key="mesa.id">
+          <ion-label>Mesa {{ mesa.numero }}</ion-label>
+          <ion-radio slot="start" :value="mesa.id"></ion-radio>
+        </ion-item>
+      </ion-radio-group>
+    </ion-list>
+  </template>
+  
+  <script setup>
+  import { defineProps, ref, defineEmits } from 'vue';
+  
+  const props = defineProps({
+    mesasDisponibles: Array
+  });
+  
+  const emit = defineEmits(['update:mesa']);
+  const mesaSeleccionada = ref(null);
+  </script>
+  
