97306ae472fe64f012defdc467cd9b67ba355337
carlosleal16 <cmleal-2023a@corhuila.edu.co>
2025-04-23 01:25:43 -0500

    Componente de ingreso de datos del cliente

---
+++ app/app/src/components/DatosCliente.vue
@@ -0,0 +1,25 @@
+<template>
+    <ion-list>
+      <ion-item>
+        <ion-label position="floating">Nombre</ion-label>
+        <ion-input v-model="nombre"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Contacto</ion-label>
+        <ion-input v-model="contacto"></ion-input>
+      </ion-item>
+    </ion-list>
+  </template>
+  
+  <script setup>
+  import { ref, defineEmits, watch } from 'vue';
+  
+  const emit = defineEmits(['update:cliente']);
+  const nombre = ref('');
+  const contacto = ref('');
+  
+  watch([nombre, contacto], () => {
+    emit('update:cliente', { nombre: nombre.value, contacto: contacto.value });
+  });
+  </script>
+  
